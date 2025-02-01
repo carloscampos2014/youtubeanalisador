3013070c77369896e6e51fc181c07b9430944c78
Carlos Alberto de Lima Campos <carlos.campos.pro@prosoft.com.br>
2025-02-01 20:21:51 -0300

    feat: Versão para publicação

--- app.py
+++ app.py
@@ -172,7 +172,6 @@ def get_video_transcript(video_id):
         return " ".join([entry['text'] for entry in transcript])
     except Exception:
         return ""
-
 def process_text(text, num_words):
     """Processa o texto e retorna as palavras mais frequentes, formatadas com centralização e numeração."""
     words = re.findall(r'\b\w+\b', text.lower())
@@ -184,35 +183,49 @@ def process_text(text, num_words):
     df = pd.DataFrame(most_common, columns=["Palavra", "Quantidade"])
     df["Percentual"] = (df["Quantidade"] / total_words) * 100
     df.index = df.index + 1
-    
+
+    # Formatando as colunas Quantidade e Percentual
     df['Quantidade'] = df['Quantidade'].apply(lambda x: locale.format_string("%d", x, grouping=True))
     df['Percentual'] = df['Percentual'].apply(lambda x: locale.format_string("%.2f%%", x))
-    
-    df_styled = df.style.set_properties(subset=['Quantidade', 'Percentual'], **{'text-align': 'center'})
-    df_styled = df_styled.set_properties(subset=['Palavra'], **{'text-align': 'left'})
-    df_styled = df_styled.set_table_styles({
-        'data': [{'selector': 'th', 'props': [('text-align', 'center')]},
-                 {'selector': 'td', 'props': [('text-align', 'center')]}]
-    }).set_table_attributes('style="width:90%;"')
-    
-    return df_styled
 
+    # Definindo estilo para a tabela
+    df_styled = df.style.set_properties(**{'text-align': 'center'})
+    df_styled = df_styled.set_properties(subset=['Palavra'], **{'text-align': 'left'})
+    df_styled = df_styled.set_table_styles([
+        {'selector': 'thead th', 'props': [('text-align', 'center')]},
+        {'selector': 'tbody th', 'props': [('text-align', 'center')]}
+    ]).set_table_attributes('style="width:90%;"')
+
+    # Definindo os títulos das colunas
+    df_styled = df_styled.set_table_styles([
+        {'selector': 'th.col_heading.level0', 'props': [('text-align', 'center')]},
+        {'selector': 'th.col_heading.level1', 'props': [('text-align', 'center')]},
+        {'selector': 'td.row_heading.level0', 'props': [('text-align', 'center')]}
+    ])
+
+    return df_styled
 def main():
     st.sidebar.title("Configurações")
     channel_url = st.sidebar.text_input("Insira a URL do canal do YouTube:", "", placeholder="Exemplo: https://www.youtube.com/@canaltragicomico/")
     st.sidebar.write("Insira a URL do canal no formato válido, por exemplo, com @handle ou ID do canal.")
     num_words = st.sidebar.number_input("Quantidade de palavras mais usadas:", min_value=1, max_value=100, value=10)
-    max_videos = st.sidebar.number_input("Quantidade de vídeos a analisar:", min_value=1, max_value=50, value=10)
+    max_videos = st.sidebar.number_input("Quantidade de vídeos a analisar:", min_value=1, max_value=20, value=2)
 
     st.title("Analisador de Palavras para Canais do YouTube")
 
     col1, col2 = st.sidebar.columns(2)
-    with col1:
-        exibir_button = st.button("Exibir na Tela")
-    with col2:
-        exportar_button = st.button("Exportar para PDF")
 
-    if exibir_button:
+    with col1:
+        run_monitor = st.button("Visualizar", key="monitor_button")
+        
+    with col2:
+        run_export = st.button("Exportar PDF", key="export_button")
+
+    # Limpar a tela ao clicar nos botões
+    if run_monitor or run_export:
+        st.empty()
+
+    if run_monitor:
         placeholder = st.empty()
         placeholder.markdown("""
             <div class="spinner-container">
@@ -260,10 +273,12 @@ def main():
             dislikes_text = f" | Dislikes: {locale.format_string('%d', int(details['dislikes']), grouping=True)}" if details['dislikes'] != 'N/A' else ""
             st.write(f"### {index}. {title}")
             st.write(f"Data: {published_at} | Views: {locale.format_string('%d', int(details['views']), grouping=True)} | Likes: {locale.format_string('%d', int(details['likes']), grouping=True)}{dislikes_text}")
-            st.markdown(df.to_html(), unsafe_allow_html=True)
+            st.markdown(df.set_table_styles([
+                {'selector': 'thead th', 'props': [('text-align', 'center')]},
+                {'selector': 'tbody th', 'props': [('text-align', 'center')]}
+            ]).to_html(), unsafe_allow_html=True)
 
-    if exportar_button:
-        st.title("Analisador de Palavras para Canais do YouTube")  # Limpar o conteúdo deixando o título
+    if run_export:
         placeholder = st.empty()
         placeholder.markdown("""
             <div class="spinner-container">
@@ -276,7 +291,7 @@ def main():
 
         if not video_data:
             st.error("Não foi possível obter vídeos do canal.")
-            placeholder.empty()  # Remover o spinner em caso de erro
+            placeholder.empty()
             return
 
         all_text = ""
@@ -289,34 +304,50 @@ def main():
                     <div class="spinner"></div>
                     <div class="spinner-text">Analisando {index}º Vídeo com Título: {title}...</div>
             """, unsafe_allow_html=True)
-            
+
             transcript = get_video_transcript(video_id)
             details = get_video_details(video_id)
             all_text += " " + transcript
-            video_df = process_text(transcript, num_words)
+            video_df = process_text(transcript, num_words).data
             video_results.append((index, video_id, title, published_at, details, video_df))
 
             dislikes_text = f" | Dislikes: {locale.format_string('%d', int(details['dislikes']), grouping=True)}" if details['dislikes'] != 'N/A' else ""
 
-            # Adiciona o conteúdo para o PDF
             pdf_content.append(f"{index}. {title}")
             pdf_content.append(f"Data: {published_at} | Views: {locale.format_string('%d', int(details['views']), grouping=True)} | Likes: {locale.format_string('%d', int(details['likes']), grouping=True)}{dislikes_text}")
-            pdf_content.append(video_df.data.to_string(index=False) + "\n")  # Acesse diretamente o DataFrame com video_df.data
 
-        # Adiciona as informações do canal ao PDF
+            # Adicionar a tabela de resultados ao PDF
+            table_data = [['#', 'Palavra', 'Quantidade', 'Percentual']]
+            for idx, row in enumerate(video_df.itertuples(index=False, name=None), start=1):
+                table_data.append([idx, row[0], row[1], row[2]])
+            
+            table = Table(table_data, colWidths=[20, 240, 100, 100])
+            table.setStyle(TableStyle([
+                ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+                ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+                ('ALIGN', (0, 0), (0, -1), 'CENTER'),  # Alinhar coluna 'Palavra' à esquerda
+                ('ALIGN', (1, 0), (1, -1), 'LEFT'),
+                ('ALIGN', (2, 0), (-1, -1), 'CENTER'),
+                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+                ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ]))
+            pdf_content.append(table)
+
         if channel_info:
             pdf_content.insert(0, f"Quantidade de vídeos: {locale.format_string('%d', int(channel_info['videoCount']), grouping=True)}")
             pdf_content.insert(0, f"Inscritos: {locale.format_string('%d', int(channel_info['subscriberCount']), grouping=True)}")
             pdf_content.insert(0, f"**Descrição:** {channel_info['description']}")
             pdf_content.insert(0, f"Informações do canal: {channel_info['title']}")
 
-        placeholder.empty()  # Remover o spinner após o processamento
+        placeholder.empty()
 
-        # Gerar o PDF
         pdf_file_path = "resultados.pdf"
         generate_pdf(pdf_content, pdf_file_path)
+
         with open(pdf_file_path, "rb") as f:
-            st.download_button("Exportar para PDF", f, file_name="resultados.pdf", mime="application/pdf")
+            st.download_button("Baixar PDF", f, file_name="resultados.pdf", mime="application/pdf")
 
 if __name__ == "__main__":
     main()
